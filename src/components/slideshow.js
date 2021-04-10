1250261d6b3f02305b7fd1967bee3f5f52c9ac87
Ben <ben.lynch1931@gmail.com>
2021-04-11 00:01:10 +0100

    change slideshow text color to white

--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -17,19 +17,19 @@ export default function Slideshow() {
         <div className='each-slide'>
           <div style={{height: '25vw', width: '80vw', backgroundImage: `url(${slideImages[0]})` }}>
           {/* <img src={require('../assets/testPic1.jpeg')} style={{ width: '80vw' }}/> */}
-            <span>Slide 1</span>
+            <span style={{ color: '#FFFFFF' }}>Slide 1</span>
           </div>
         </div>
         <div className='each-slide'>
           <div style={{height: '25vw', width: '80vw', backgroundImage: `url(${slideImages[1]})` }}>
             {/* <img src={require('../assets/testPic2.jpeg')} style={{ width: '80vw' }}/> */}
-            <span>Slide 2</span>
+            <span style={{ color: '#FFFFFF' }}>Slide 2</span>
           </div>
         </div>
         <div className='each-slide'>
           <div style={{height: '25vw', width: '80vw', backgroundImage: `url(${slideImages[2]})` }}>
             {/* <img src={require('../assets/testPic3.jpeg')} style={{ width: '80vw' }}/> */}
-            <span>Slide 3</span>
+            <span style={{ color: '#FFFFFF' }}>Slide 3</span>
           </div>
         </div>
       </Slide>
