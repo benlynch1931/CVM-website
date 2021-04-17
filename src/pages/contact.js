a21641dd78de09aa7854e2e8716da5e97ad55a03
Ben <ben.lynch1931@gmail.com>
2021-04-17 15:20:04 +0100

    add draft contact info to page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -23,8 +23,22 @@ export default function Index() {
 
 
       <div className="main-contact">
-        <h1>Contact</h1>
-        <p>For general enquiries, please contact Kate Lynch at </p>
+        <h1 style={{ textAlign: 'center' }}>Contact</h1>
+        <br />
+
+        <p>For general enquiries, please contact Anne Higgs at **Email@example.com**</p>
+
+        <p><strong>Brass</strong></p>
+        <p>Anne Higgs: **Email@example.com**</p>
+        <br />
+        <p><strong>Voice</strong></p>
+        <p>Kate Lynch: **Email@example.com**</p>
+        <br />
+        <p><strong>Strings</strong></p>
+        <p>Anne Higgs: **Email@example.com**</p>
+        <br />
+        <p><strong>Percussion</strong></p>
+        <p>Anne Higgs: **Email@example.com**</p>
       </div>
 
       <Footer />
